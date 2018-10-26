c808d25856335eaa0079d344cc8aad5da38cd5a3
TamerMograbi <tomispk@gmail.com>
2018-10-26 16:33:21 -0400

    Added character with basic movement
    got a character from unity asset store.
    character can be moved on the ground but animation isn't smooth.
    
    need to add rotation or decide how we want the character to move. (mouse and keyboard? or just keyboard?)

---
+++ RotoWorld/Assets/AnimController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimController : MonoBehaviour
+{
+
+    public Animator anim;
+    private Rigidbody rb;
+    float speed;
+    public float moveVertical;
+    public float moveHorizontal;
+    float maxJump;
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        speed = 2;
+        moveVertical = 0;
+        moveHorizontal = 0;
+        maxJump = 8;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Level1");
+        }
+    }
+    private void FixedUpdate()
+    {
+        moveVertical = Input.GetAxis("Vertical");
+        moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        if (moveVertical != 0 || moveHorizontal != 0)
+        {
+            //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
+            //Vector3 faceRot = Vector3.Normalize(movement);
+            //if (faceRot != Vector3.zero)
+            //{
+            //    transform.forward = faceRot;
+            //}
+            anim.Play("run");
+        }
+        else
+        {
+            anim.Play("idle");
+        }
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        
+    }
+}
