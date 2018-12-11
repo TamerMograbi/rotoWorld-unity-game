d4c68bcdd6fed88507ceec38a33c5c3478d16322
JayRes109 <justinayres97@gmail.com>
2018-12-11 15:22:47 -0500

    Revert "Merge branch 'master' of https://github.com/TamerMograbi/rotoWorldCMSC425"
    This reverts commit 3c97f75da84f65497464ede43cd031a6123ca541, reversing
    changes made to b235ab1e32c7b6dcb58c861ff27f9df2829dc539.

--- RotoWorld/Assets/Scripts/AnimController.cs
+++ RotoWorld/Assets/Scripts/AnimController.cs
@@ -328,6 +328,7 @@ public class AnimController : MonoBehaviour
                 transform.eulerAngles = new Vector3(transform.eulerAngles.x, 0, 90);
             }*/
         }
+        transform.localScale = new Vector3(1, 1, 1);
 
         // -------- Throw Rock -----------
         if (Input.GetKeyDown(KeyCode.LeftControl) && anim.GetInteger("state") == 0 && IsGrounded())
@@ -395,6 +396,7 @@ public class AnimController : MonoBehaviour
         {
             rb.AddForce(getJumpDirectionVector()*6, ForceMode.Impulse);
             yield return new WaitForSeconds(0.5f);
+            FindObjectOfType<AudioManager>().Play("warp");
         }
         gravityDir = dir;
     }
